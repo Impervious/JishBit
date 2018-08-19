b774a50dc286bed8fbdc577c02d5d224ead2ea81
cback <cbackasSE@gmail.com>
2018-08-18 20:02:19 -0500

    Just a simple thing to make synclounging a more fun experience for everyone

---
+++ src/main/java/jish/commands/CommandRoom.java
@@ -0,0 +1,40 @@
+package jish.commands;
+
+import jish.JishBit;
+import jish.Util;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+
+public class CommandRoom implements Command {
+    @Override
+    public String getName() {
+        return "room";
+    }
+
+    @Override
+    public void execute(JishBit bot, IDiscordClient client, String[] args, IGuild guild, IMessage msg, boolean isPrivate) {
+        Util.botLog(msg);
+
+        String reply;
+        if (args.length == 0) {
+            reply = "Click here to join your SnycLounge room:\n" + buildURL("");
+        } else if (args.length == 1) {
+            reply = "Click here to join your SnycLounge room:\n" + buildURL(args[0]);
+        } else {
+            reply = "Error u broke me";
+        }
+
+        Util.sendEmbed(msg.getChannel(), reply);
+
+        Util.deleteMessage(msg);
+    }
+
+    private String buildURL(String roomName) {
+        if (roomName.isEmpty()) {
+            roomName = "ipa";
+        }
+
+        return "http://themediaserver.ddns.net:10081/ptweb/#/join?ptserver=http%3A%2F%2Fthemediaserver.ddns.net%3A10082&ptroom=" + roomName + "&owner=troyhayes50&ptpassword=pie";
+    }
+}
